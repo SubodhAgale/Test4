2db54d6368cfdd3f8f612ba7da4c613850e82499
subodhagale3112@gmail.com <subodhagale3112@gmail.com>
2025-03-13 18:18:32 +0530

    hello world

--- src/main/java/com/test4/test4/Test4Application.java
+++ src/main/java/com/test4/test4/Test4Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+
+		char ch='a';
 		SpringApplication.run(Test4Application.class, args);
 	}
 
